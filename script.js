47700da4a1924b098c85f28838d36c82f85da1b2
erlams <elmaddin@gmail.com>
2023-11-12 10:14:47 +0100

    123

---
+++ script.js
@@ -0,0 +1,23 @@
+let Azn
+let Euro = prompt('How much do you want to convert, euro to manats?')
+let Usd = prompt('How much do you want to convert, dollars to manats?')
+let ayaz;
+let murad;
+
+Usd = Azn *  1.70;
+
+let money = Usd + 'dollar = ' + Azn + 'azn' ;
+
+
+
+document.getElementById('ayaz').innerHTML = money;
+
+Euro = Azn * 2.18 ;
+
+let honey = Euro + 'euro = ' + Azn + 'azn' ;
+document.getElementById('murad').innerHTML = honey;
+
+
+
+
+
